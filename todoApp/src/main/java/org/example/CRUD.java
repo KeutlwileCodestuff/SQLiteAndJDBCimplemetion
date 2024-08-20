1afac5fd0a533af3a5e340dd06a6929487c99226
Keutlwile Mpolokeng <kmpolokeng023@student.wethinkcode.co.za>
2024-08-20 16:20:49 +0200

    add task

--- todoApp/src/main/java/org/example/CRUD.java
+++ todoApp/src/main/java/org/example/CRUD.java
@@ -5,19 +5,29 @@ import java.sql.*;
 public class CRUD {
     private static final String  URL = "jdbc:sqlite:mydatabase.db";
     String input ;
-    public void addTask(Connection connection , Statement statement){
+
+    public void addTask(Connection connection){
         String sql = "INSERT INTO (New tasks) VALUES (?)";
         try (PreparedStatement preparedState =  connection.prepareStatement(sql)){
             preparedState.setString(1 , input);
             preparedState.execute();
-            
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
-    public void removeTask(){
 
+    public void removeTask(Connection connection){
+        String sql = "DELETE FROM (New tasks) VALUES (?)";
+        try(PreparedStatement preparedState = connection.prepareStatement(sql)) {
+            preparedState.setString(1,input);
+            preparedState.execute();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+
     public void updateTask(){
 
     }
